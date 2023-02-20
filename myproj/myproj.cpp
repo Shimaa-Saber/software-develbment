bc974d7d679e10409a6b46d496273e1c63570360
Shimaa-Saber <shimaasaber812@gmail.com>
2023-02-20 17:47:22 +0200

    Added the files

---
+++ myproj/myproj.cpp
@@ -0,0 +1,16 @@
+// myproj.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    int resulte;
+    cin >> x >> y;
+    resulte = x + y;
+    cout << resulte;
+}
+
